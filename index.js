7eb9bb4cadc00dbf1ccb034bbc5920c850a91f39
Dania <daasdania97@gmail.com>
2024-07-27 21:54:40 +0300

    V4

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 //fetch('http://localhost:1234/')
-  //  .then(response => response.json())
-    //.then(json => {
-      //  console.log(json);
-      //  let students = document.getElementById('students');
-      //  json.forEach(element => {
-        //    let student = document.createElement('option');
-        //    student.innerHTML= element.name;
-        //    students.appendChild(student);
-       // })
-   // }
-   // )
-   fetch('https://restcountries.com/v3.1/all')
-      .then(response => response.json())
-      .then(json => {
-       let countries = document.getElementById('countries');
-       json.forEach(element => {
-        let country = document.createElement('div');
-        country.innerHTML= element.name.common;
-        let flags = document.createElement('img');
-       flags.src = element.flags.png;
-       country.addEventListener('click',()=>{
-        let population = document.createElement('div');
-        population.innerHTML= element.population;
-        country.appendChild(population);
-       })
-       countries.appendChild(country);
-       country.appendChild(flags);
-       });
+//  .then(response => response.json())
+//.then(json => {
+//  console.log(json);
+//  let students = document.getElementById('students');
+//  json.forEach(element => {
+//    let student = document.createElement('option');
+//    student.innerHTML= element.name;
+//    students.appendChild(student);
+// })
+// }
+// )
+fetch('https://restcountries.com/v3.1/all')
+    .then(response => response.json())
+    .then(json => {
+        let countries = document.getElementById('countries');
+        json.forEach(element => {
+            let country = document.createElement('div');
+            country.innerHTML = element.name.common;
+            let flags = document.createElement('img');
+            flags.src = element.flags.png;
+            country.addEventListener('click', () => {
+                let population = document.createElement('div');
+                population.innerHTML = element.population + 'peoples';
+                country.appendChild(population);
+
+                let week = document.createElement('div');
+                week.innerHTML = element.startOfWeek;
+                country.appendChild(week);
+            })
+            countries.appendChild(country);
+            country.appendChild(flags);
+        });
     })
