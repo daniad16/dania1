18c3758cc8545f779c9a107ced2e48c1c6187658
Dania <daasdania97@gmail.com>
2024-07-27 21:00:09 +0300

    V3

--- index.js
+++ index.js
@@ -11,14 +11,20 @@
    // }
    // )
    fetch('https://restcountries.com/v3.1/all')
-    .then(response => response.json())
-    .then(json => {
-        console.log(json);
-        let countries = document.getElementById('students');
-        json.forEach(element => {
-            let country = document.createElement('option');
-            country.innerHTML= element.name.common;
-            countries.appendChild(country);
-        })
-    }
-    )
+      .then(response => response.json())
+      .then(json => {
+       let countries = document.getElementById('countries');
+       json.forEach(element => {
+        let country = document.createElement('div');
+        country.innerHTML= element.name.common;
+        let flags = document.createElement('img');
+       flags.src = element.flags.png;
+       country.addEventListener('click',()=>{
+        let population = document.createElement('div');
+        population.innerHTML= element.population;
+        country.appendChild(population);
+       })
+       countries.appendChild(country);
+       country.appendChild(flags);
+       });
+    })
